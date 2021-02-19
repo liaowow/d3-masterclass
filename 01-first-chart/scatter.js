d2cfc22b3597b7e65624887e9c7127dea3b10d75
Annie Liao <liaowow@gmail.com>
2021-02-19 16:48:13 -0500

    draw peripherals of the scatterplot

--- 01-first-chart/scatter.js
+++ 01-first-chart/scatter.js
@@ -31,7 +31,7 @@ async function drawScatter() {
     .append("svg")
     .attr("width", dimensions.width)
     .attr("height", dimensions.height)
-    .style("border", "1px solid")
+    // .style("border", "1px dotted")
 
   const bounds = wrapper.append("g")
     .style("transform", `translate(${
@@ -61,6 +61,38 @@ async function drawScatter() {
     .attr("fill", "cornflowerblue")
     .attr("opacity", "0.5")
 
+  // 6. draw peripherals
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+  
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+    .style("transform", `translateY(${
+      dimensions.boundedHeight
+    }px)`)
+  
+  const xAxisLabel = xAxis.append("text")
+    .attr("x", dimensions.boundedWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .html("Dew point (&deg;F)")
+  
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+    .ticks(4)
+  
+  const yAxis = bounds.append("g")
+    .call(yAxisGenerator)
+  
+  const yAxisLabel = yAxis.append("text")
+    .attr("x", -dimensions.boundedHeight / 2)
+    .attr("y", -dimensions.margin.left + 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text("Relative Humidity")
+    .style("transform", "rotate(-90deg)")
+    .style("text-anchor", "middle")
 }
 
 drawScatter()
