449b4827f7648989488c29f23a202a0d72d80c71
Annie Liao <liaowow@gmail.com>
2021-02-19 09:29:07 -0500

    create scatterplot chart dimensions

--- 01-first-chart/scatter.js
+++ 01-first-chart/scatter.js
@@ -1,12 +1,30 @@
 import * as d3 from "d3";
 
 async function drawScatter() {
+  // 1. access data
   const data = await d3.json("./data/nyc_weather_data.json")
-  
   const xAccessor = d => d.dewPoint
   const yAccessor = d => d.humidity
   
-  console.log(yAccessor(data[0]))
+  // 2. create chart dimensions
+  const width = d3.min([
+    window.innerWith * 0.9,
+    window.innerHeight * 0.9,
+  ])
+  const dimensions = {
+    width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    }
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.right - dimensions.margin.left
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top - dimensions.margin.bottom
 }
 
 drawScatter()
